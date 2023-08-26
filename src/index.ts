434ed68178770fb82f35ae297c3b39367719b1ee
shubham sharma <shubham.sharma2@oodles.io>
2023-08-26 17:05:30 +0530

    primsa setup and created mutation

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import bodyParser from 'body-parser';
+import { prismaCient } from './lib/db';
 async function init() {
     const app = express();
     app.use(bodyParser.json())
@@ -14,11 +15,41 @@ async function init() {
             hello:String
             say(name:String): String
         }
+        
+        type Mutation {
+            createUser(firstName:String!,lastName:String!,email:String!,password:String!): Boolean
+        }
     ` ,//schema
         resolvers: {
             Query:{
                 hello : ()=> `Hey there i'm a graphql server`,
-                say :(_,{name}:{name:string})=> `hey ${name}, how are you?`
+                say :
+                (_,{name}:{name:string})=> `hey ${name}, how are you?`
+            },
+            Mutation:{
+                createUser: async (_, 
+                { 
+                    firstName, 
+                    lastName, 
+                    email, 
+                    password
+                }: {
+                    firstName: string; 
+                    lastName: string; 
+                    email: string; 
+                    password: string; 
+                }) => { 
+                        await prismaCient.user.create({
+                            data:{
+                                email,
+                                lastName,
+                                firstName,
+                                password,
+                                salt:"random_salt",
+                            },
+                    });
+                    return true
+                }
             }
         }
     })
